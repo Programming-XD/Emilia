abde1e72812ae1a327f0d738bc23c36f5ddc7a2a
ᴀᴋɪᴛᴏ <fr.programmer.xd@gmail.com>
2025-09-03 13:24:16 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -8,26 +8,26 @@ def get_user_list(config, key):
 
 
 class Config(object):
-    API_HASH = "45aabfac" # API_HASH from my.telegram.org
-    API_ID = 62 # API_ID from my.telegram.org
+    API_HASH = "f4815b9a16cb03c2f5eabe8db1cb0903" # API_HASH from my.telegram.org
+    API_ID = 18990697 # API_ID from my.telegram.org
 
-    BOT_ID = 521 # BOT_ID
-    BOT_USERNAME = "Elf_Robot" # BOT_USERNAME
+    BOT_ID = 7011123946 # BOT_ID
+    BOT_USERNAME = "KakashiHatakeXD_Bot" # BOT_USERNAME
 
-    MONGO_DB_URL = "mongodb://username:password@localhost:27017/emi?directConnection=true&authSource=admin" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://publicDB:publicDBbyKira@public.twckcqf.mongodb.net" # MongoDB URL from MongoDB Atlas
 
-    SUPPORT_CHAT = "SpiralTechDivision" # Support Chat Username
-    UPDATE_CHANNEL = "SpiralUpdates" # Update Channel Username
+    SUPPORT_CHAT = "FOS_Community" # Support Chat Username
+    UPDATE_CHANNEL = "FOREST_OF_SAVIOUR" # Update Channel Username
     START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp" # Start Image
-    DEV_USERS = [6040984893, 6461051572, 7107018652] # Dev Users
-    TOKEN = "57375" # Bot Token from @BotFather
+    DEV_USERS = [6632519077] # Dev Users
+    TOKEN = "7011123946:AAH6OMw44iRmGFBObURfZNcPP8xQEZM1JMw" # Bot Token from @BotFather
     CLONE_LIMIT = 50 # Number of clones your bot can make
 
     EVENT_LOGS = -10093 # Event Logs Chat ID
-    OWNER_ID = 6040984893 # Owner ID
+    OWNER_ID = 6632519077
  
     TEMP_DOWNLOAD_DIRECTORY = "./" # Temporary Download Directory
-    BOT_NAME = "Emilia" # Bot Name
+    BOT_NAME = "Kakashi" # Bot Name
     WALL_API = "6950f53" # Wall API from wall.alphacoders.com
 
 
